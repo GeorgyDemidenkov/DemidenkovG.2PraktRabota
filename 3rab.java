a730cab0ad25c55f7f57fe8ccc6b965e5ebcd0be
GeorgyDemidenkov <98282737+GeorgyDemidenkov@users.noreply.github.com>
2024-03-28 21:05:33 +0300

    Create 3rab.java

---
+++ 3rab.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Введите что-то:");
+        String inputString=scanner.nextLine();
+        System.out.println(removeVowels(inputString));
+    }
+    public static String removeVowels(String inputString){
+        return inputString.replaceAll("[aeyuioAEYUIO]","");
+    }
+}
